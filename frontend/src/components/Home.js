a8efd357f09d0cd33720ed712de51641670f2745
jyahn <jooyoungxahn@gmail.com>
2019-11-12 09:55:04 -0800

    added functionality to fetch posts

---
+++ frontend/src/components/Home.js
@@ -0,0 +1,12 @@
+import React, { Component, Fragment } from "react";
+import PostList from '../containers/PostList';
+
+class Home extends Component {
+  render() {
+    return (
+      <PostList />
+    );
+  }
+}
+
+export default Home;
---
+++ frontend/src/containers/PostList.js
@@ -0,0 +1,46 @@
+import React, { Component, Fragment } from "react";
+import { connect } from 'react-redux';
+import { fetchTitlesFromAPI } from '../actions/titles';
+import { Link } from 'react-router-dom';
+import './PostList.css'
+
+
+
+class PostList extends Component {
+  async componentDidMount() {
+    if (this.props.titles.length === 0) {
+      await this.props.fetchTitlesFromAPI();
+    }
+  }
+  render() {
+    console.log("props in postlist", this.props)
+    return (
+      <div className="postsContainer row">
+        {this.props.titles.map(title => (
+          <Fragment>
+            <div className="post">
+              <div className="title">
+                <h2>{title.title}</h2>
+              </div>
+              <div className="dogPic" style={{ backgroundImage: `url(${title.dog_pic})` }} />
+              <h5>{title.description}</h5>
+            </div>
+          </Fragment>
+        ))}
+      </div>
+    );
+  }
+}
+
+function mapStateToProps(state) {
+  return {
+    titles: state.titles,
+  }
+}
+
+export default connect(
+  mapStateToProps,
+  { fetchTitlesFromAPI }
+)(PostList);
+
+{/* <img className="dogPic" src={title.dog_pic} /> */ }

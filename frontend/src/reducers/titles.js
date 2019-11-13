93480b513dee6630e097ca9f1c64725dc65fed3c
jyahn <jooyoungxahn@gmail.com>
2019-11-13 11:27:25 -0800

    implemented logic to have titles sorted by votes

--- frontend/src/reducers/titles.js
+++ frontend/src/reducers/titles.js
@@ -5,6 +5,11 @@ import {
 } from "../actions/types";
 
 
+function sortByVote(posts) {
+  return posts.sort((a, b) => b.votes - a.votes);
+}
+
+
 function makeTitleFromPost({ id, title, description, dog_pic, votes }) {
   return { id, title, description, dog_pic, votes };
 }
@@ -13,13 +18,13 @@ export default function rootReducer(state = [], action) {
   switch (action.type) {
 
     case FETCH_TITLES:
-      return [...action.titles];
+      return sortByVote([...action.titles]);
 
     case ADD_POST:
-      return [...state, makeTitleFromPost(action.post)];
+      return sortByVote([...state, makeTitleFromPost(action.post)]);
 
     case VOTE:
-      return state.map(title => title.id === action.postId ? { ...title, votes: action.votes } : title);
+      return sortByVote(state.map(title => title.id === action.postId ? { ...title, votes: action.votes } : title));
 
     default:
       return state;

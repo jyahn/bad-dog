78e6e71383f83b3977e58820c39bd25c451b441e
Diana Khuong <diana.t.khuong@gmail.com>
2019-11-13 15:56:00 -0800

    Form styling WIP

--- frontend/src/components/NewPostForm.js
+++ frontend/src/components/NewPostForm.js
@@ -1,10 +1,9 @@
 import React, { Component } from "react";
-import './NewPostForm.css';
-
+import "./NewPostForm.css";
 
 class NewPostForm extends Component {
   static defaultProps = {
-    post: { title: "", description: "", dog_pic: "" },
+    post: { title: "", description: "", dog_pic: "" }
   };
 
   constructor(props) {
@@ -12,7 +11,7 @@ class NewPostForm extends Component {
     this.state = {
       title: this.props.post.title,
       description: this.props.post.description,
-      dog_pic: this.props.post.dog_pic,
+      dog_pic: this.props.post.dog_pic
     };
 
     this.handleSubmit = this.handleSubmit.bind(this);
@@ -30,42 +29,128 @@ class NewPostForm extends Component {
 
   render() {
     return (
-      <div className="text-center">
-        <h1>New Bad Dog</h1>
-        <form className="col-lg-6 offset-3" onSubmit={this.handleSubmit}>
-
-          <div className="form-group">
-            <label htmlFor="editform-title">Title:</label>
-            <input onChange={this.handleChange}
-              id="editform-title"
-              name="title"
-              className="form-control"
-              value={this.state.title} />
+      <>
+        <main>
+          <figure>
+            <picture>
+              <source
+                media="(min-width: 768px)"
+                srcset="//my-assets.netlify.com/codepen/dailyui-001/img.png     340w,
+                            //my-assets.netlify.com/codepen/dailyui-001/img@2x.png  680w,
+                            //my-assets.netlify.com/codepen/dailyui-001/img@3x.png 1020w"
+                sizes="360px"
+              />
+              <source
+                srcset="//my-assets.netlify.com/codepen/dailyui-001/img-small.png     340w,
+                            //my-assets.netlify.com/codepen/dailyui-001/img-small@2x.png  680w,
+                            //my-assets.netlify.com/codepen/dailyui-001/img-small@3x.png 1020w"
+                sizes="(min-width: 320px) 290px,
+                            (min-width: 480px) 435px
+                            (min-width: 640px) 580px"
+              />
+              <img
+                src="//my-assets.netlify.com/codepen/dailyui-001/img.png"
+                alt="Citymap illustration"
+              />
+            </picture>
+          </figure>
+          <div className="headline">
+            <h2 class="text-headline">Vino Blanco</h2>
           </div>
+          <form onSubmit={this.handleSubmit}>
+            {/* <div>
+              <h1>
+              New Bad Dog
+              </h1>
+            </div> */}
+            <span>
+              <input
+                class="text-body"
+                id="title"
+                name="title"
+                type="text"
+                placeholder="Title"
+                required
+                onChange={this.handleChange}
+                value={this.state.title}
+              />
+            </span>
+            <span>
+              <input
+                class="text-body"
+                id="editform-description"
+                name="description"
+                type="text"
+                placeholder="I'm a bad dog because"
+                required
+                onChange={this.handleChange}
+                value={this.state.description}
+              />
+            </span>
+            <span>
+              <input
+                class="text-body"
+                id="city"
+                name="city"
+                type="text"
+                required
+              />
+            </span>
+            <input
+              class="text-small-uppercase"
+              id="submit"
+              type="submit"
+              value="Subscribe now"
+            />
+          </form>
+        </main>
 
-          <div className="form-group">
-            <label htmlFor="editform-description">I'm a bad dog because:</label>
-            <input onChange={this.handleChange}
-              id="editform-description"
-              name="description"
-              className="form-control"
-              value={this.state.description} />
-          </div>
+        <div className="text-center">
+          <h1>New Bad Dog</h1>
+          <form className="col-lg-6 offset-3" onSubmit={this.handleSubmit}>
+            {/* <div className="form-group">
+              <label htmlFor="editform-title">Title:</label>
+              <input
+                onChange={this.handleChange}
+                id="editform-title"
+                name="title"
+                className="form-control"
+                value={this.state.title}
+              />
+            </div> */}
 
-          <div className="form-group">
-            <label htmlFor="editform-dogpic">Image:</label>
-            <input onChange={this.handleChange}
-              id="editform-dogpic"
-              name="dog_pic"
-              type="url"
-              className="form-control"
-              value={this.state.dog_pic} />
-          </div>
+            {/* <div className="form-group">
+              <label htmlFor="editform-description">
+                I'm a bad dog because:
+              </label>
+              <input
+                onChange={this.handleChange}
+                id="editform-description"
+                name="description"
+                className="form-control"
+                value={this.state.description}
+              />
+            </div> */}
 
-          <button className="btn btn-primary mr-2">Save</button>
-          <button onClick={this.props.cancel} className="btn btn-secondary">Cancel</button>
-        </form>
-      </div>
+            <div className="form-group">
+              <label htmlFor="editform-dogpic">Image:</label>
+              <input
+                onChange={this.handleChange}
+                id="editform-dogpic"
+                name="dog_pic"
+                type="url"
+                className="form-control"
+                value={this.state.dog_pic}
+              />
+            </div>
+
+            <button className="btn btn-primary mr-2">Save</button>
+            <button onClick={this.props.cancel} className="btn btn-secondary">
+              Cancel
+            </button>
+          </form>
+        </div>
+      </>
     );
   }
 }

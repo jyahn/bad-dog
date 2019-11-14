bcf006a04dc0dabff6799d7bc63986d1a1bb3d37
Diana Khuong <diana.t.khuong@gmail.com>
2019-11-14 14:06:44 -0800

    navbar styling

--- frontend/src/components/Nav.js
+++ frontend/src/components/Nav.js
@@ -1,21 +1,28 @@
 import React, { Component } from "react";
 import { NavLink } from "react-router-dom";
+import "./bad-dog-logo.png";
 
-import './Nav.css';
+import "./Nav.css";
 
 class Nav extends Component {
   render() {
     return (
-      <div className="App container">
-        <header className="App-header jumbotron mt-2">
-          <h1 className="App-title display-4">Bad Dog! <i className="Nav-paw fas fa-paw"></i></h1>
-          <p className="lead">A place to publicly shame your disobedient dogs.</p>
-          <nav>
-            <NavLink exact to="/">Home</NavLink>
-            <NavLink exact to="/new">Submit a bad dog</NavLink>
-          </nav>
-        </header>
-      </div>
+      <nav className="navbar">
+        <NavLink exact to="/">
+          <a className="logo navbar-brand">
+            <img
+              src={require("./bad-dog-logo.png")}
+              class="d-inline-block align-top"
+              className="bad-dog-logo"
+              alt="bad-dog-logo"
+            />
+            Bad Dog!
+          </a>
+        </NavLink>
+        <NavLink exact to="/new">
+              <i className="new" class="far fa-edit"></i>
+        </NavLink>
+      </nav>
     );
   }
 }
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -13,6 +13,7 @@ import { composeWithDevTools } from 'redux-devtools-extension';
 
 
 
+
 const store = createStore(
   rootReducer,
   composeWithDevTools(

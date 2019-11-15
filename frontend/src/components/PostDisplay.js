f60e58e54d9afaf8c8bb62d6278377bde5e773cd
jyahn <jooyoungxahn@gmail.com>
2019-11-14 22:10:29 -0800

    styling improvements

--- frontend/src/components/PostDisplay.js
+++ frontend/src/components/PostDisplay.js
@@ -20,51 +20,36 @@ class PostDisplay extends Component {
     const { title, description, dog_pic, votes } = this.props.post;
     return (
       <div className="PostDisplay">
-        <div className="card">
-          <div className="card-body">
-            <div
-              className="dogPic"
-              style={{ backgroundImage: `url(${dog_pic})` }}
-            >
-              {/* <div className="card-text"> */}
+        <div className = "Post">
+          <div className="body">
+            <div className="dogPic"
+              style={{ backgroundImage: `url(${dog_pic})` }}>
               <div className="description mx-auto px-5">
                 {description} <i class="fas fa-paw"></i>
               </div>
-              <div className="footer">
-                <span className="title-footer">{title}</span>
-                <span className="votes-footer">
-                  <small>{votes} votes</small>
-                  <i
-                    className={
-                      localStorage.getItem(this.props.post.id) === "down"
-                        ? "grayedThumb fas fa-thumbs-up text-success ml-2 mt-2"
-                        : "fas fa-thumbs-up text-success ml-2 mt-2"
-                    }
-                    onClick={() => this.doVoteUp("up", title.id)}
-                  />
-                  <i
-                    className={
-                      localStorage.getItem(this.props.post.id) === "up"
-                        ? "grayedThumb fas fa-thumbs-down text-danger ml-2 mt-2"
-                        : "fas fa-thumbs-down text-danger ml-2 mt-2"
-                    }
-                    onClick={() => this.doVoteDown("down", title.id)}
-                  />
-                </span>
-              </div>
+            </div>
+            <div className="footer">
+              <span className="title-footer">{title}</span>
+              <span className="votes-footer">
+                <small>{votes} votes</small>
+                <i className={
+                  localStorage.getItem(this.props.post.id) === "down"
+                    ? "grayedThumb fas fa-thumbs-up text-success ml-2 mt-2"
+                    : "fas fa-thumbs-up text-success ml-2 mt-2"
+                }
+                  onClick={() => this.doVoteUp("up", title.id)}
+                />
+                <i className={
+                  localStorage.getItem(this.props.post.id) === "up"
+                    ? "grayedThumb fas fa-thumbs-down text-danger ml-2 mt-2"
+                    : "fas fa-thumbs-down text-danger ml-2 mt-2"
+                }
+                  onClick={() => this.doVoteDown("down", title.id)}
+                />
+              </span>
             </div>
           </div>
         </div>
-        <br />
-        {/* Edit a post */}
-        {/* <div className="PostDisplay-right">
-          <div className="PostDisplay-edit-area">
-            <i className="fas fa-edit text-primary"
-              onClick={this.props.toggleEdit} />
-            <i className="fas fa-times text-danger"
-              onClick={this.props.delete} />
-          </div>
-        </div> */}
       </div>
     );
   }
